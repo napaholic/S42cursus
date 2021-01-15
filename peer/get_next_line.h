f913603294ef7542f06c342b488e1e1bf0727bce
Jaewook Kim <jaewkim@c1r1s4.42seoul.kr>
2021-01-15 22:31:38 +0900

    peer

---
+++ peer/get_next_line.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clim <clim@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/13 13:12:23 by clim              #+#    #+#             */
+/*   Updated: 2021/01/15 18:41:43 by clim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 500
+# endif
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 256
+# endif
+
+int				get_next_line(int fd, char **line);
+
+size_t			ft_strlen(const char *s);
+
+char			*ft_strdup(const char *s1);
+
+char			*ft_strjoin(char *s1, char *s2);
+
+size_t			ft_strlcpy(char *dest, const char *src, size_t dstsize);
+
+size_t			ft_strlcat(char *dest, const char *src, size_t dstsize);
+
+#endif
