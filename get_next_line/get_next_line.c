60161b03dc167928d49b253aeeefadf98daaced4
leno <kimja5163@gmail.com>
2021-01-22 10:51:32 +0900

    netlast

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jaewkim <jaewkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/14 17:37:56 by jaewkim           #+#    #+#             */
-/*   Updated: 2021/01/19 20:24:49 by jaewkim          ###   ########.fr       */
+/*   Updated: 2021/01/22 10:29:32 by jaewkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,61 @@
 
 int					chk_newline(char *str)
 {
-	int				rst;
+	int				idx;
 
-	rst = 0;
-	while (*str)
+	idx = 0;
+	while (str[idx])
 	{
-		if (*str++ == '\n')
-			rst = 1;
+		if (str[idx] == '\n')
+			return (idx);
+		++idx;
 	}
-	return (rst);
+	return (-1);
+}
+
+int				splited_line(char **backup, int idx_str, char **line)
+{
+	char		*tmp;
+
+	(*backup)[idx_str] = '\0';
+	*line = ft_strdup(*backup);
+	if (!ft_strlen(&(*backup)[idx_str + 1]))
+	{
+		free(*backup);
+		*backup = NULL;
+		return (1);
+	}
+	tmp = ft_strdup(&(*backup)[idx_str + 1]);
+	free(*backup);
+	*backup = tmp;
+
+	return (1);
+}
+
+int					read_after_all(char **backup, int size_read, char **line)
+{
+	int				idx;
+
+	
+	return 0;
 }
 
 int					get_next_line(int fd, char **line)
 {
-	static char		*prevbuf[OPEN_MAX];
+	static char		*backup[OPEN_MAX];
 	char			buf[BUFFER_SIZE + 1];
 	char			*tmp;
-	int				byte_read;
-	int				idx;
+	int				size_read;
+	int				idx_str;
 
 	if ((fd < 0) || (line == NULL) || (BUFFER_SIZE <= 0))
 		return (-1);
-	while ((byte_read = read(fd, buf, BUFFER_SIZE)) > 0)
+	while ((size_read = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
-		
+		buf[size_read] = '\0';
+		backup[fd] = ft_strjoin(backup[fd], buf);
+		if ((idx_str = chk_newline(backup[fd])) >= 0)
+			return (splited_line(backup[fd], idx_str, *line));
 	}
 	return (0);
 }
